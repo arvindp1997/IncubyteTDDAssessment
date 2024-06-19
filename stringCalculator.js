a4e5705db52058736cdd1d13d6e52b5a9d59c418
arvind97veative <aravind.pandit@veative.com>
2024-06-19 08:40:46 +0530

    added multiple delimiter case and multiple delimiter with variable length case

--- stringCalculator.js
+++ stringCalculator.js
@@ -1,31 +1,60 @@
-let functionInvokeCount = 0
+const getDelimiters = (str) => {
+  let allDelimiters = [];
+  let del = "";
+  let delStart = false;
+  for (let i = 0; i < str.length; i++) {
+    if (str[i - 1] === "[") {
+      del += str[i];
+      delStart = true;
+    } else if (delStart && str[i] !== "]") del += str[i];
+    if (str[i] === "]") {
+      allDelimiters.push(del);
+      del = "";
+      delStart = false;
+    }
+  }
+  return allDelimiters;
+};
+
+let functionInvokeCount = 0;
 const add = (numbers) => {
-    functionInvokeCount++
-    //console.log({ functionInvokeCount})
+  functionInvokeCount++;
+  //console.log({ functionInvokeCount})
   let isNegativePresent = false;
   let delimiter = ",";
   if (numbers.includes("//")) {
-    if(numbers[2] !== '['){
-        delimiter = numbers[2];
-        numbers = numbers.slice(3);
-        console.log(numbers)
+    if (numbers[2] !== "[") {
+      delimiter = numbers[2];
+      numbers = numbers.slice(3);
+      console.log(numbers);
     } else {
+      const isMultipleDelimiterPresent =
+        numbers.split("").filter((char) => char === "[").length > 1;
+      if (!isMultipleDelimiterPresent) {
         numbers = numbers.slice(2);
-        let del = ''
-        let i = 1
-        while(numbers[i] !== ']'){
-            del+=numbers[i]
-            i++
+        let del = "";
+        let i = 1;
+        while (numbers[i] !== "]") {
+          del += numbers[i];
+          i++;
         }
-        delimiter = del
+        delimiter = del;
         numbers = numbers.slice(i);
+      } else {
+        const allDelimiters = getDelimiters(numbers);
+        allDelimiters.forEach((del) => {
+          numbers = numbers.replaceAll(del, delimiter);
+        });
+      }
     }
- 
   }
   if (numbers.includes("\n")) {
     numbers = numbers.replaceAll("\n", delimiter);
   }
-  const nums = numbers.split(delimiter).map((num) => Number(num)).filter((num) => num <= 1000)
+  const nums = numbers
+    .split(delimiter)
+    .map((num) => Number(num))
+    .filter((num) => num <= 1000);
 
   nums.forEach((num) => {
     if (num < 0) {
--- stringCalculator.test.js
+++ stringCalculator.test.js
@@ -1,67 +1,53 @@
-const stringCalculator = require('./stringCalculator')
+const stringCalculator = require("./stringCalculator");
 
-test('empty string', () => {
-    expect(
-        stringCalculator('')
-     ).toBe(0)
-})
+test("empty string", () => {
+  expect(stringCalculator("")).toBe(0);
+});
 
-test('one number', () => {
-    expect(
-        stringCalculator('1')
-     ).toBe(1)
-})
+test("one number", () => {
+  expect(stringCalculator("1")).toBe(1);
+});
 
-test('two number', () => {
-    expect(
-        stringCalculator('1,5')
-     ).toBe(6)
-})
+test("two number", () => {
+  expect(stringCalculator("1,5")).toBe(6);
+});
 
-test('any amount of numbers', () => {
-    expect(
-        stringCalculator('1,2,3,4,5')
-     ).toBe(15)
-})
+test("any amount of numbers", () => {
+  expect(stringCalculator("1,2,3,4,5")).toBe(15);
+});
 
-test('\n as delimiter', () => {
-    expect(
-        stringCalculator('1\n2,3')
-     ).toBe(6)
-})
+test("\n as delimiter", () => {
+  expect(stringCalculator("1\n2,3")).toBe(6);
+});
 
-test('support any delimiter, ; as delimiter', () => {
-    expect(
-        stringCalculator('//;\n1;2')
-     ).toBe(3)
-})
+test("support any delimiter, ; as delimiter", () => {
+  expect(stringCalculator("//;\n1;2")).toBe(3);
+});
 
-test('support any delimiter, $ as delimiter', () => {
-    expect(
-        stringCalculator('//$\n1$2$5')
-     ).toBe(8)
-})
+test("support any delimiter, $ as delimiter", () => {
+  expect(stringCalculator("//$\n1$2$5")).toBe(8);
+});
 
-test('single negative case', () => {
-    expect(
-        stringCalculator('-1,2,3')
-     ).toBe(0)
-})
+test("single negative case", () => {
+  expect(stringCalculator("-1,2,3")).toBe(0);
+});
 
-test('Multiple negative case', () => {
-    expect(
-        stringCalculator('-1,-2,3')
-     ).toBe(0)
-})
+test("Multiple negative case", () => {
+  expect(stringCalculator("-1,-2,3")).toBe(0);
+});
 
-test('Ignore number bigger than 1000', () => {
-    expect(
-        stringCalculator('2,1001')
-     ).toBe(2)
-})
+test("Ignore number bigger than 1000", () => {
+  expect(stringCalculator("2,1001")).toBe(2);
+});
 
-test('Variable length delimiter case', () => {
-    expect(
-        stringCalculator('//[***]\n1***2***3')
-     ).toBe(6)
-})
+test("Variable length delimiter case", () => {
+  expect(stringCalculator("//[***]\n1***2***3")).toBe(6);
+});
+
+test("Multiple delimiter case", () => {
+  expect(stringCalculator("//[*][%]\n1*2%3")).toBe(6);
+});
+
+test("Multiple delimiter with variable length case", () => {
+  expect(stringCalculator("//[**][%%]\n1**2%%3")).toBe(6);
+});
